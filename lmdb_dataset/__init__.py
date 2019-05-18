12b3a0db6cc5808e9de8b6678419b6f714dd9cf8
Ziyang Hu <hu.ziyang@cantab.net>
2019-05-18 10:01:49 +0800

    parallel map

--- lmdb_dataset/__init__.py
+++ lmdb_dataset/__init__.py
@@ -1,2 +1,3 @@
 from .dataset import LMDBDataset
 from .writer import LMDBDatasetWriter
+from .utils import parallel_map
--- lmdb_dataset/utils.py
+++ lmdb_dataset/utils.py
@@ -1,2 +1,20 @@
+from multiprocessing.pool import Pool
+from threading import Semaphore
+
+
 def encode_key(c):
     return ('%012d' % c).encode('ascii')
+
+
+def parallel_map(iterable, func, n_procs, *, chunksize=1, buffer_ratio=2, **pool_args):
+    semaphore = Semaphore(int(chunksize * n_procs * buffer_ratio))
+
+    def pre_throttle(it):
+        for x in it:
+            semaphore.acquire()
+            yield x
+
+    with Pool(processes=n_procs, **pool_args) as pool:
+        for result in pool.imap(func, pre_throttle(iterable), chunksize=chunksize):
+            semaphore.release()
+            yield result
