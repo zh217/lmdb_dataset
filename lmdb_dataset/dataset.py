2d814a1445c92a75fb2cc7afffd8390385152d6c
Ziyang Hu <hu.ziyang@cantab.net>
2019-05-20 16:49:55 +0800

    limit and offset

--- lmdb_dataset/dataset.py
+++ lmdb_dataset/dataset.py
@@ -9,7 +9,7 @@ from .utils import encode_key
 
 
 class LMDBDataset(Dataset):
-    def __init__(self, db_path, subset_n=None, readahead=True, restart_every=-1):
+    def __init__(self, db_path, readahead=True, restart_every=-1, limit=-1, offset=0):
         super().__init__()
         self.db_path = db_path
         self.use_count = 0
@@ -19,13 +19,16 @@ class LMDBDataset(Dataset):
         self.db = None
 
         with self.get_db().begin(write=False) as txn:
-            self.length = pyarrow.deserialize(txn.get(b'__len__'))
+            self.length = int(pyarrow.deserialize(txn.get(b'__len__')))
             try:
                 self.keys = pyarrow.deserialize(txn.get(b'__keys__'))
             except:
                 self.keys = None  # [txt_utils.encode_key(i) for i in range(self.length)]
-        if subset_n:
-            self.length = subset_n
+        self.limit = limit
+        if offset < 0:
+            self.offset = self.length + offset
+        else:
+            self.offset = offset
 
     def get_db(self):
         if self.use_count == self.restart_every:
@@ -48,14 +51,17 @@ class LMDBDataset(Dataset):
     def __getitem__(self, index):
         with self.get_db().begin(write=False) as txn:
             if self.keys:
-                k = self.keys[index]
+                k = self.keys[index + self.offset]
             else:
-                k = encode_key(index)
+                k = encode_key(index + self.offset)
             byteflow = txn.get(k)
         return pyarrow.deserialize(byteflow)
 
     def __len__(self):
-        return self.length
+        l = self.length - self.offset
+        if self.limit >= 0:
+            l = min(self.limit, l)
+        return l
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' + self.db_path + ')'
