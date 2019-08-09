516443dc897180915083cf2bf2e729fb42b791d4
Ziyang Hu <hu.ziyang@cantab.net>
2019-08-09 13:55:48 +0800

    offset in more places

--- lmdb_dataset/concat.py
+++ lmdb_dataset/concat.py
@@ -2,18 +2,19 @@ from torch.utils.data import Dataset
 
 
 class InterleaveDataset(Dataset):
-    def __init__(self, *datasets):
+    def __init__(self, *datasets, offset=0):
         super().__init__()
         self.datasets = list(datasets)
         self.lengths = [len(d) for d in self.datasets]
-        self.length = sum(self.lengths)
+        self.length = sum(self.lengths) - offset
+        self.offset = offset
         self.indices = []
         for i, l in enumerate(self.lengths):
             self.indices += list((i, k) for k in range(l))
         self.indices.sort(key=lambda mi: (mi[1] / self.lengths[mi[0]], mi[0]))
 
     def __getitem__(self, index):
-        db_idx, item_idx = self.indices[index]
+        db_idx, item_idx = self.indices[index + self.offset]
         return self.datasets[db_idx][item_idx]
 
     def __len__(self):
@@ -24,18 +25,19 @@ class InterleaveDataset(Dataset):
 
 
 class ConcatDataset(Dataset):
-    def __init__(self, *datasets):
+    def __init__(self, *datasets, offset=0):
         super().__init__()
         self.datasets = list(datasets)
         self.lengths = [len(d) for d in self.datasets]
-        self.length = sum(self.lengths)
+        self.length = sum(self.lengths) - offset
+        self.offset = offset
         self.indices = []
         for i, l in enumerate(self.lengths):
             self.indices += list((i, k) for k in range(l))
         self.last_db = None
 
     def __getitem__(self, index):
-        db_idx, item_idx = self.indices[index]
+        db_idx, item_idx = self.indices[index + self.offset]
         cur_db = self.datasets[db_idx]
         if self.last_db and self.last_db != cur_db:
             self.last_db.close()
